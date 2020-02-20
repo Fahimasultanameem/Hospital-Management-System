2d18f05c767b33937cd1331503c9c2e6e38cb22b
Fahima Sultana Meem <FSMeem>
2020-02-20 12:57:06 +0600

    hospital management system

---
+++ aboutus.php
@@ -0,0 +1,79 @@
+<?php
+include("header.php");
+?>
+<div class="wrapper col2">
+  <div id="gallery">
+    <ul>
+      <div id="galleryDisplay">
+        <li class="placeholder" style="background-image:url(images/demo/img1.jpg); background-repeat: no-repeat; background-size: 100% 349px;"></li>
+      </div>
+      <div style="height:100%; width:32%; float:right;">
+        <div style="width:100%; height:30%; margin:0px 0px 2px 0px; border-bottom:4px double #DADADA ;">
+          <li><a class="swap" style="background-image:url(images/demo/img3.jpg);background-repeat: no-repeat; background-size: 100% 105px;" href="#gallery"><span><img src="images/demo/image3.jpg" style="height: 100%; width: 100%;" alt="" /></span></a></li>
+        </div>
+        <div style="width:100%; height:30%; margin:0px 0px 2px 2px; border-bottom:4px double #DADADA;">
+          <li><a class="swap" style="background-image:url(images/demo/img1.jpg);background-repeat: no-repeat; background-size: 100% 105px;" href="#gallery"><span><img src="images/demo/image1.jpg" style="height: 100%; width: 100%;" alt="" /></span></a></li>
+        </div>
+        <div style="width:100%; height:30%; margin:0px 0px 2px 2px; border-bottom:4px double #DADADA;">
+          <li class="last"><a class="swap" style="background-image:url(images/demo/img2.jpg);background-repeat: no-repeat; background-size: 100% 105px;" href="#gallery"><span><img src="images/demo/image2.jpg" style="height: 100%; width: 100%;" alt="" /></span></a></li> 
+        </div>
+        
+        
+        
+      </div>
+      
+    </ul>
+    <div class="clear"></div>
+  </div>
+</div>
+<div class="wrapper col4">
+  <div id="container" style="background-color:#e6f8fe;padding: 0px 20px 0px 20px; ">
+    <div id="content">
+      <h1>About Bangladesh Hospital</h1>
+      <p align="justify">Bangladesh Hospitals Dhaka is the only JCI Accredited 425-bed multi-disciplinary super-specialty tertiary care hospital in Bangladesh, confidently providing comprehensive health care with the latest medical, surgical and diagnostic facilities. These services are provided by expert medical professionals, skilled nurses and technologists using state-of-the-art technology. The hospital started its operation on April 16, 2005 with a mission to provide high quality international standard healthcare which will meet the needs and exceed the expectations of the people of Bangladesh. It is an eleven-storied building built on four acres of land with 435,000 square feet floor space, which was constructed with assistance of renowned architecture company Smith Group, USA. The hospital is a joint project of Bangladesh Hospitals Enterprise Limited and STS Holdings Limited (STS Group).</p>
+      <p align="justify">Bangladesh Hospitals Dhaka has all the characteristics of a world-class hospital with wide range of services and specialists, equipments and technology, ambience and service quality. The hospital is a showcase of synergy of medical technology and advances in IT through paperless medical records. The skilled nurses, technologists and administrators of Bangladesh Hospitals Dhaka, aided by state-of-the-art equipments, provide a congenial infrastructure for the medical professionals in providing healthcare of international standards.</p>
+      <p align="justify"><q>Since its inception to date, approximately 6,60,116 patients have registered with Bangladesh Hospitals Dhaka and over 25,14,152 patients have availed out-patient consultation across over 29 disciplines. The hospital is the first and only in Bangladesh to get accredited by Joint Commission International (JCI), which reassures its continued commitment to the highest quality international standard of healthcare in the region. We are not only about cutting-edge technology and performance-driven skill of our doctors and nurses, but we are also about hope, as symbolized by the flames of hope in the torch on our logo.</p>
+      <div class="homecontent" style="height:300px;" >
+        <ul>
+          <div style="width:50%; height:100%;float: right; overflow: auto;">
+            <li>
+              <div style="width:100%; height:80%;"><img src="images/spciality.jpg" alt="" style="width:100%;height:100%;"/></div>
+              <div style="width:100%; height:20%;font-size:15px; font-weight:bold; color:#666666; line-height:normal;text-align: center;">Super Specialty Hospital</div>
+            </li>
+          </div>
+          <div style="width:50%; height:100%; float: left; overflow: auto;">
+            <li class="last">
+             <div style="width:100%; height:80%;"><img src="images/24x7.png" alt="" style="width:100%;height:100%;"/></div>
+             <div style="width:100%; height:20%;font-size:15px; font-weight:bold; color:#666666; line-height:normal;text-align: center;">24X7 services</div>
+          </li>
+          </div>
+        </ul>
+        <div class="clear"></div>
+      </div>
+      <p align="justify"><strong>Directors message:</strong><br />
+      Bangladesh Hospitals Dhaka is a joint project of Bangladesh Hospitals Enterprise Limited and STS Holdings Limited (STS Group). Bangladesh Hospitals Enterprise Limited is the leading private sector healthcare provider in Asia. It owns and manages a network of speciality hospitals and clinics, a chain of pharmacy retail outlets across India, and provides consultancy services for commissioning and managing the speciality hospitals.</p>
+    </div>
+    <div id="column">
+      <div id="featured">
+        <ul>
+          <li>
+            <h2>Our Services</h2>
+            <p class="imgholder"><img src="images/SDM-Image-5.png" alt="" style="width:240px;height:90px;" /></p>
+          </li>
+          <li aria-haspopup="Sidebar1_Menu2:submenu:36" role="menuitem">Diagnostic Services</li>
+          <li role="menuitem">Consultation</li>
+          <li role="menuitem">In Patient Services</li>
+          <li role="menuitem">Health Checkup Packages</li>
+          <li role="menuitem">24 X 7 Services</li>
+          <li role="menuitem">Physiotherapy Services</li>
+          <li role="menuitem"><a href=""  tabindex="-1">Dialysis</a></li>
+          <li role="menuitem"><a href=""  tabindex="-1">Insurance Schemes</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="clear"></div>
+  </div>
+</div>
+<?php
+include("footer.php");
+?>
---
+++ index.php
@@ -0,0 +1,80 @@
+<?php
+include("header.php");
+?>
+<div class="wrapper col1">
+  <div id="container">
+
+    <div id="content" style="align-content: center; width:100%;">
+
+      <div class="homecontent1" style="width:100%; float:left; word-wrap: break-word;">
+          <h1 style="font-weight: bold; font-size: 21px; text-align: center;">Welcome to Online Hospital Management System</h1>
+          <p align="justify">  The goal of the Doctor appointment SYSTEM
+      System is to improve the efficiency of the health by reducing the overall time and cost used to create documents and retrieve information.
+    .</p>
+          <p align="justify">The main feature of E-Knowledge in Doctor appointment system is to provide the browser to get appointments from a doctor through internet instead of going there and fixing an appointment..</p>
+          <p align="justify"> Everyone needs to have Medical attention at any time. So we allow every user to register freely at any time..</p>
+          <p>Doctor appointment System maintains patient’s prescriptions so that their medical details are always available in Internet, which will be more convenient for the patients. This will be more comfortable for the patient..</p>
+      </div>
+      
+
+      <div class="homecontent" style="width:100%;padding:0; float:right;">
+        <ul>
+          <div style="background-image:url(images/homepage-image.jpg); background-repeat: no-repeat; background-size: 100% 100%; padding: 100px 0px 100px 0px;width:100%; float:right;">
+             
+              <div style="background-color:rgba(230, 247, 255,0.4); width:30%; height:200px; float:right;"> 
+                  <h2>Book An Appointment </h2>
+                  <p>for new patient please register yourself here.</p>
+              </div>
+              
+              <div style="width:10%; height:200px; float:right;"><div style="float:right; width: 0; height:0; border-top:100px solid transparent; border-right: 200px solid #e6f7ff; border-bottom:100px solid transparent; opacity:0.4;" ></div></div>  
+              
+              <div style="width:40%; height:200px; float:left;">
+                <a href="patientappointment.php">
+                  <div style="width:50%; height:100%; margin-right:25%; margin-left:25%;border-radius: 50%; align-content: center; border:10px double #035a7d;">
+                    <div style="background-color: #035a7d; height: 90%; width: 90%;margin-top:5%; margin-left:5%;text-align: center; border-radius: 50%;">
+                      <div style="width:50%; height:50%; border:2px solid transparent; margin:0 auto; padding:40% 20% 0% 20%; color:#9be1fd; font-size: 25px;">
+                        Book Now
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            
+          </div>
+         
+
+
+          <div style="background-image:url(images/homepage-image.jpg); background-repeat: no-repeat; background-size: 100% 100%; margin-top: 20px; width:100%; padding: 100px 0px 100px 0px; float:left;">
+            
+              <div style="background-color:rgba(230, 247, 255,0.4); width:30%; height:200px; float:left;">
+                <h2>Login Panel</h2>
+                <p> for existing patients 
+              </div>
+
+              <div style="width:10%; height:200px; float:left;"><div style="float:left; width: 0; height:0; border-top:100px solid transparent; border-left: 200px solid #e6f7ff; border-bottom:100px solid transparent; opacity:0.4;"></div></div>
+
+              <div style="width:40%; height:200px; float:right;">
+                <a href="patientlogin.php">
+                  <div style="width:50%; height:100%; margin-right:25%; margin-left:25%;border-radius: 50%; align-content: center; border:10px double #035a7d;">
+                    <div style="background-color:#035a7d; height: 90%; width: 90%;margin-top:5%; margin-left:5%;text-align: center; border-radius: 50%;">
+                      <div style="width:50%; height:50%; border:2px solid transparent; margin:0 auto; padding:40% 20% 0% 20%; color:#9be1fd; font-size: 25px;">
+                        LOG IN
+                      </div>
+                    </div>
+                  </div>
+
+                </a>
+              </div>
+             
+          </div>
+        </ul>
+        <div class="clear"></div>
+      </div>
+    </div>
+   
+    <div class="clear"></div>
+  </div>
+</div>
+<?php
+include("footer.php");
+?>
